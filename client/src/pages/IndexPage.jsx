0346cbc9eec0696797ca977e166fb05313f84ffa
Thanh Le <thanhlevan0203@gmail.com>
2023-03-12 00:56:22 -0800

    create LoginPage

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Header } from '../Header'
+
+const IndexPage = () => {
+  return (
+    <div>
+      index page 
+    </div>
+  )
+}
+
+export default IndexPage
