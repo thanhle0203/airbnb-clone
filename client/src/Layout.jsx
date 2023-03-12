27e6b4a4e6fc044bebb0f5bc01914ce37d3e98f9
Thanh Le <thanhlevan0203@gmail.com>
2023-03-12 15:14:18 -0700

    create register page

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -5,7 +5,7 @@ import { Header } from './Header'
 
 export const Layout = () => {
   return (
-    <div className='p-4'>
+    <div className='p-4 flex flex-col min-h-screen'>
         <Header />
         <Outlet />
     </div>
