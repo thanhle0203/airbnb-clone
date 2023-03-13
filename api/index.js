4537dbcd4d04799a930b4863b7511a280c429849
Thanh Le <thanhlevan0203@gmail.com>
2023-03-12 17:08:10 -0700

    create user module

--- api/index.js
+++ api/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const User = require('./models/User.js')
+require('dotenv').config()
 const app = express();
 
 
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: 'http://127.0.0.1:5173/'
+    origin: 'http://127.0.0.1:5173'
 }))
+
+console.log(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL);
 
 app.get('/test', (req, res) => {
     res.json('test.ok');
@@ -15,6 +21,9 @@ app.get('/test', (req, res) => {
 
 app.post('/register', (req,res) => {
     const {name,email,password} = req.body;
+    User.create({
+        name, email, password,
+    })
     res.json({name,email,password});
 })
 
