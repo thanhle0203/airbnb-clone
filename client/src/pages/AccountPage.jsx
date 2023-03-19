1db9f39766be61b2dbef5e1b5fbb5163c63946c9
Thanh Le <thanhlevan0203@gmail.com>
2023-03-18 17:16:59 -0700

    create place Page

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -28,9 +28,11 @@ let { subpage } = useParams();
   }
 
   function linkClasses(type = null) {
-    let classes = 'py-2 px-6';
+    let classes = 'inline-flex gap-1 py-2 px-6 rounded-full';
     if (type === subpage) {
-      classes += ' bg-primary text-white rounded-full';
+      classes += ' bg-primary text-white ';
+    } else {
+      classes += ' bg-gray-200';
     }
     return classes;
   }
